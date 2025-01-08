af7fa1708f304b385c2445a38c4cc2df5a3570e7
merinpriyasha <merinpriyasha@gmail.com>
2025-01-08 13:20:39 +0530

    setting up backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    const { email, password, name } = req.body;
+	try {
+		const userExists = await User.findOne({ email });
+
+		if (userExists) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+		const user = await User.create({ name, email, password });
+
+        res.status(201).json({user, message: "User created successfully"})
+
+    } catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ message: error.message });
+	}
+}
+
+export const login = async (req, res) => {
+    res.send("Login route called");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout route called");
+}
